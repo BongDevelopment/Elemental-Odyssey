d0bc546007ae27ed01dba01652f19537d65e1d6b
Bongo <prestonplayz112343@gmail.com>
2024-03-22 22:19:18 -0500

    ♻️ refactored

--- src/buttons/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    data: {
-      name: "test",
-      description: "nuck figgers",
-    },
-    async execute(interaction, client) {
-      console.log("button pressed");
-    },
-  };
