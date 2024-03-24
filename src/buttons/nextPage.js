2a89909fb749c0f19bb09e83422e159aad73a88c
Bongo <prestonplayz112343@gmail.com>
2024-03-24 18:35:41 -0500

    💄 shop buttons

---
+++ src/buttons/nextPage.js
@@ -0,0 +1,10 @@
+module.exports = {
+    data: {
+      name: "nextPage",
+      description: "moves page",
+    },
+    async execute(interaction, client) {
+      console.log("button pressed");
+      interaction.reply('page button clicked');
+    },
+  };
---
+++ src/buttons/two.js
@@ -0,0 +1,10 @@
+module.exports = {
+    data: {
+      name: "two",
+      description: "purchase an item",
+    },
+    async execute(interaction, client) {
+      console.log("button pressed");
+      interaction.reply('purchase button clicked');
+    },
+  };
