125a82c5b824d77765713b013ce9747caa890cfd
Bongo <prestonplayz112343@gmail.com>
2024-03-30 22:29:02 -0500

    🐛 fixed import issue

--- src/util/buttonHandler.js
+++ src/util/buttonHandler.js
@@ -1,34 +1,36 @@
-import { readdirSync } from 'fs'
-import path from 'path'
-import { Collection } from 'discord.js'
-import { ChalkAdvanced } from 'chalk-advanced'
+import { fileURLToPath } from 'url'; 
+
+import { readdirSync } from 'fs';
+import path from 'path';
+import { Collection } from 'discord.js';
+import { ChalkAdvanced } from 'chalk-advanced';
 
 export default class ButtonHandler {
-	constructor(client) {
-		this.client = client
-		this.client.buttons = new Collection()
-	}
+    constructor(client) {
+        this.client = client;
+        this.client.buttons = new Collection();
+    }
 
-	//  Load the buttons
+    // Load the buttons
 
-	async load() {
-		for (const file of readdirSync(
-			path.join(import.meta.dirname, '..', 'buttons')
-		).filter((file) => file.endsWith('.js'))) {
-			const button = (await import(`../buttons/${file}`)).default
-			this.client.buttons.set(button.data.name, button)
-		}
-		console.log(
-			`${ChalkAdvanced.white('Elemental Odyssey')} ${ChalkAdvanced.gray(
-				'>'
-			)} ${ChalkAdvanced.green('Successfully loaded buttons')}`
-		)
-	}
+    async load() {
+        const currentFilePath = fileURLToPath(import.meta.url);
+        const currentDirectory = path.dirname(currentFilePath);
+        const buttonsDirectory = path.join(currentDirectory, '..', 'buttons');
 
-	// Reload the buttons
+        for (const file of readdirSync(buttonsDirectory).filter((file) => file.endsWith('.js'))) {
+            const button = (await import(`file://${path.join(buttonsDirectory, file)}`)).default;
+            this.client.buttons.set(button.data.name, button);
+        }
+        console.log(
+            `${ChalkAdvanced.white('Elemental Odyssey')} ${ChalkAdvanced.gray('>')} ${ChalkAdvanced.green('Successfully loaded buttons')}`
+        );
+    }
 
-	reload() {
-		this.client.buttons = new Collection()
-		this.load()
-	}
+    // Reload the buttons
+
+    reload() {
+        this.client.buttons = new Collection();
+        this.load();
+    }
 }
